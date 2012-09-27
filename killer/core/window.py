195ae50b5ac119ad16cd73fbd35d4141c912dd33
Kenet Jervet <newelevenken@yahoo.com.cn>
2012-09-27 14:42:44 +0800

    Okay, Win32 API working now.
    Signed-off-by: Kenet Jervet <newelevenken@yahoo.com.cn>

--- killer/core/window.py
+++ killer/core/window.py
@@ -4,26 +4,22 @@ Created on 2012-9-26
 @author: kj
 '''
 
-from module_handler import delete_module
 from sys_env import SysEnv
-from util.decorators import static_variable
-import subprocess
-import re
-import ctypes
+from ctypes import *
 
-@static_variable('so', ctypes.CDLL('./external/xwininfo.so'))
 def pick_window():
-    window_id = pick_window.so.getwindow_main();
-    p = subprocess.Popen([
-                        'xprop',
-                        '_NET_WM_PID',
-                        '-id',
-                        str(window_id),
-                        ], stdout=subprocess.PIPE)
-    prepared_process_id = p.communicate()[0]
-    reg_matches = re.match(r'_NET_WM_PID\(CARDINAL\) = (\d+)', prepared_process_id)
-    if reg_matches:
-        process_id = reg_matches.group(1)
-        return process_id
-    else:
-        return -1
+    x = input("Input X coordinate:")
+    y = input("Input Y coordinate:")
+
+    user32 = windll.user32
+
+    window_id = user32.WindowFromPoint(x,y)
+    print window_id
+    
+    process_id = c_long()
+    user32.GetWindowThreadProcessId(window_id, byref(process_id))
+    
+    return process_id.value if process_id > 0 else -1
+
+if __name__ == '__main__':
+    print pick_window()
