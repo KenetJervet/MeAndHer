0f0dd34151cce10d967b1bf63cf08c05e0e06816
Kenet Jervet <newelevenken@yahoo.com.cn>
2012-09-27 13:31:50 +0800

    Stupid `call`, we have built-in module `os` and `kill` system call to do that.
    Signed-off-by: Kenet Jervet <newelevenken@yahoo.com.cn>

--- killer/core/process.py
+++ killer/core/process.py
@@ -5,11 +5,10 @@ Created on 2012-9-26
 '''
 
 from subprocess import call
+import signal
+import os
 from sys_env import SysEnv
 import wx
 
 def kill_process(process_id):
-    if call(['kill', '-9', str(process_id)]) == 0:
-        pass
-    else:
-        wx.MessageBox('Internal error.', 'Process ID', wx.OK)
+    os.kill(int(process_id), signal.SIGTERM)
